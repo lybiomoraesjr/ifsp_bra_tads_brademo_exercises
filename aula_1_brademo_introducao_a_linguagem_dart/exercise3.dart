ab1f194ffdefccdaaa20a320e2e111c05600e57a
Lybio Moraes Junior <j.lybio@aluno.ifsp.edu.br>
2025-03-15 21:35:24 -0300

    feat: add exercise 3

---
+++ aula_1_brademo_introducao_a_linguagem_dart/exercise3.dart
@@ -0,0 +1,31 @@
+class Animal {
+  int _id;
+  String _nome;
+  String _cor;
+
+  Animal(this._id, this._nome, this._cor);
+
+  void imprimirDetalhes() {
+    print("Id: ${_id}");
+    print("Nome: ${_nome}");
+    print("Cor: ${_cor}");
+  }
+}
+
+class Cat extends Animal {
+  String _som;
+
+  Cat(int id, String nome, String cor, this._som) : super(id, nome, cor);
+
+  void imprimirDetalhes() {
+    super.imprimirDetalhes();
+    print("Som: ${_som}");
+  }
+}
+
+void main() {
+  Cat gato1 = new Cat(1, "Frajola", "Preto e branco", "Miau");
+
+  print("Gato 1:");
+  gato1.imprimirDetalhes();
+}
