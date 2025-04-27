e4c386ee5c4750aca2b12a73ae99e9a9a0f1b8e4
Lybio Moraes Junior <j.lybio@aluno.ifsp.edu.br>
2025-04-27 16:03:04 -0300

    feat: add exercise 6

--- atividade_slides_aula_05_pdmi6_ criacao_de_layouts_com_flutter/ex6/lib/main.dart
+++ atividade_slides_aula_05_pdmi6_ criacao_de_layouts_com_flutter/ex6/lib/main.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widgets/button_section.dart';
+import 'package:flutter_application_1/widgets/image_section.dart';
+import 'package:flutter_application_1/widgets/text_section.dart';
+import 'package:flutter_application_1/widgets/title_section.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    const String appTitle = 'Ex. 6  -  Flutter Layout';
+    return MaterialApp(
+      title: appTitle,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(title: const Text(appTitle)),
+        body: const Center(
+          child: Column(
+            children: [
+              ImageSection(image: 'images/lake.webp'),
+              TitleSection(
+                name: "Oeschinen Lake Campground",
+                location: "Kandersteg, Switzerland",
+              ),
+              ButtonSection(),
+              TextSection(
+                description:
+                    'Lake Oeschinen lies at the foot of the Blüemlisalp in the '
+                    'Bernese Alps. Situated 1,578 meters above sea level, it '
+                    'is one of the larger Alpine Lakes. A gondola ride from '
+                    'Kandersteg, followed by a half-hour walk through pastures '
+                    'and pine forest, leads you to the lake, which warms to 20 '
+                    'degrees Celsius in the summer. Activities enjoyed here '
+                    'include rowing, and riding the summer toboggan run.',
+              ),
+            ],
+          ),
         ),
       ),
     );
